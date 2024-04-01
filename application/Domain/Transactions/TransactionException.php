17e2b2ac98c9dd7a3892d0a1bfbf5ae1cce68a4d
Wilson Lavrador <wilsonmlavrador@gmail.com>
2024-04-01 16:34:36 -0300

    feat: Adicionado novas exceções ao arquivo de Exceção do dominio Transaction

--- application/Domain/Transactions/TransactionException.php
+++ application/Domain/Transactions/TransactionException.php
@@ -4,8 +4,28 @@ namespace TheWallet\Transactions;
 
 class TransactionException extends \Exception
 {
-    public static function transactionNotAllowedByThirdy()
+    public static function transactionNotAllowedByThirdy(): self
     {
+        return new self('Your Transaction was not authorized.');
+    }
 
+    public static function companiesCannotTransfer(): self
+    {
+        return new self('Your profile is that of a Company and for now it is not allowed to send values.');
+    }
+
+    public static function negativeValuesNotAllowed(): self
+    {
+        return new self('Negative Values Not Allowed.');
+    }
+
+    public static function hasNoBalance(): self
+    {
+        return new self('You do not have a balance to make the transfer.');
+    }
+
+    public static function matchingWalletsNotAllowed(): self
+    {
+        return new self('You cannot send funds to your own wallet.');
     }
 }
