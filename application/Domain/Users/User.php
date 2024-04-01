7bdf40d3f8bd5850cb21bdaa5f3aa76f929b4500
Wilson Lavrador <wilsonmlavrador@gmail.com>
2024-04-01 03:32:00 -0300

    hotfix: refatoração da entidade Users, para o domínio

---
+++ application/Domain/Users/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace TheWallet\Users;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use TheWallet\Users\Enum\UserTypeEnum;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, HasUuids;
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'document_number',
+        'user_type',
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+            'user_type' => UserTypeEnum::class
+        ];
+    }
+
+    public static function newFactory(): UserFactory
+    {
+        return UserFactory::new();
+    }
+
+    public function wallet(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
+}
--- application/database/seeders/DatabaseSeeder.php
+++ application/database/seeders/DatabaseSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use TheWallet\Users\User;
 
 class DatabaseSeeder extends Seeder
 {
