93b5619e6a9e1962cfacc3db334595a107d18e39
Wilson Lavrador <wilsonmlavrador@gmail.com>
2024-04-01 22:45:20 -0300

    feature: * Implementação da ValidationException na transaction exception

--- application/Domain/Transactions/TransactionException.php
+++ application/Domain/Transactions/TransactionException.php
@@ -2,30 +2,34 @@
 
 namespace TheWallet\Transactions;
 
-class TransactionException extends \Exception
+use Illuminate\Validation\ValidationException;
+
+class TransactionException extends ValidationException
 {
     public static function transactionNotAllowedByThirdy(): self
     {
-        return new self('Your Transaction was not authorized.');
+        return self::withMessages(['Your Transaction was not authorized by Thirdy.']);
     }
 
     public static function companiesCannotTransfer(): self
     {
-        return new self('Your profile is that of a Company and for now it is not allowed to send values.');
+        return self::withMessages([
+            'Your profile is that of a Company and for now it is not allowed to send values.'
+        ]);
     }
 
     public static function negativeValuesNotAllowed(): self
     {
-        return new self('Negative Values Not Allowed.');
+        return self::withMessages(['Negative Values Not Allowed.']);
     }
 
     public static function hasNoBalance(): self
     {
-        return new self('You do not have a balance to make the transfer.');
+        return self::withMessages(['You do not have a balance to make the transfer.']);
     }
 
     public static function matchingWalletsNotAllowed(): self
     {
-        return new self('You cannot send funds to your own wallet.');
+        return self::withMessages(['You cannot send funds to your own wallet.']);
     }
 }
