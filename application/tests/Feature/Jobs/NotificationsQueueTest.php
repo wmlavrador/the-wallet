21adf37ea32b79636ccb7a5deb0f09bba5839bdd
Wilson Lavrador <wilsonmlavrador@gmail.com>
2024-04-02 00:26:25 -0300

    feature: criação do teste da NotificationQueue

---
+++ application/tests/Feature/Jobs/NotificationsQueueTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Jobs;
+
+use Illuminate\Support\Facades\Queue;
+use PHPUnit\Framework\MockObject\MockObject;
+use Tests\TestCase;
+use TheWallet\Jobs\NotificationsQueue;
+use TheWallet\Notifications\NotificationContract;
+use TheWallet\Notifications\NotificationsException;
+
+class NotificationsQueueTest extends TestCase
+{
+    private NotificationsQueue $notificationsQueue;
+
+    private MockObject $notificationServiceMock;
+
+    protected function setUp(): void
+    {
+        $this->notificationServiceMock = $this->getMockBuilder(NotificationContract::class)->getMock();
+        $this->notificationsQueue = new NotificationsQueue('Test message');
+    }
+
+    public function testHandleCallsNotificationServiceWithTextMessage()
+    {
+        $this->notificationServiceMock->expects($this->once())
+            ->method('sendText')
+            ->with('Test message')
+            ->willReturn(true);
+
+        $this->notificationsQueue->handle($this->notificationServiceMock);
+    }
+
+    public function testHandleThrowsExceptionWhenNotificationNotSent()
+    {
+        $this->notificationServiceMock->expects($this->once())
+            ->method('sendText')
+            ->willReturn(false);
+
+        $this->expectException(NotificationsException::class);
+        $this->expectExceptionMessage('Notification service not responding, tray again.');
+
+        $this->notificationsQueue->handle($this->notificationServiceMock);
+    }
+}
--- application/tests/Unit/Transactions/UseCases/CreateTransactionUseCaseTest.php
+++ application/tests/Unit/Transactions/UseCases/CreateTransactionUseCaseTest.php
@@ -3,16 +3,18 @@
 namespace Tests\Unit\Transactions\UseCases;
 
 use Tests\TestCase;
+use TheWallet\Jobs\NotificationsQueue;
 use TheWallet\PaymentsAuthorizer\PaymentsAuthorizerContract;
 use TheWallet\Transactions\Repository\TransactionRepository;
+use TheWallet\Transactions\TransactionException;
 use TheWallet\Transactions\UseCases\CreateTransactionUseCase;
 use TheWallet\Transactions\DataTransferObject\TransactionData;
+use TheWallet\Users\Enum\UserTypeEnum;
 use TheWallet\Wallets\Repository\WalletRepository;
 use TheWallet\Wallets\Wallet;
 
 class CreateTransactionUseCaseTest extends TestCase
 {
-    private TransactionRepository $transactionRepositoryMock;
     private WalletRepository $walletRepositoryMock;
     private PaymentsAuthorizerContract $paymentAuthorizerMock;
     private CreateTransactionUseCase $transactionUseCaseMock;
@@ -21,33 +23,33 @@ class CreateTransactionUseCaseTest extends TestCase
     {
         parent::setUp();
 
-        $this->transactionRepositoryMock = $this->createMock(TransactionRepository::class);
+        $transactionRepositoryMock = $this->createMock(TransactionRepository::class);
         $this->walletRepositoryMock = $this->createMock(WalletRepository::class);
         $this->paymentAuthorizerMock = $this->createMock(PaymentsAuthorizerContract::class);
 
+        $this->notificationsQueueMock = $this->getMockBuilder(NotificationsQueue::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $this->transactionUseCaseMock = new CreateTransactionUseCase(
-            $this->transactionRepositoryMock,
+            $transactionRepositoryMock,
             $this->walletRepositoryMock,
             $this->paymentAuthorizerMock
         );
     }
 
-    public function testCreateTransaction()
+    public function testTransactionIsNotAuthorizedByThirdy()
     {
-        $this->transactionRepositoryMock->expects($this->once())
-            ->method('createTransaction');
-
         $this->paymentAuthorizerMock->method('isAuthorizerPayment')
-            ->willReturn(true);
+            ->willReturn(false);
+
+        $walletSender = new Wallet();
+        $walletSender->balance = 1999;
+        $walletSender->user = (object) ['user_type' => UserTypeEnum::Customer->value];
+        $walletSender->id = fake()->uuid();
 
         $this->walletRepositoryMock->method('getWalletById')
-            ->willReturn((new Wallet()));
-
-        $this->walletRepositoryMock->expects($this->exactly(1))
-            ->method('decreaseBalance');
-
-        $this->walletRepositoryMock->expects($this->exactly(1))
-            ->method('increaseBalance');
+            ->willReturn($walletSender);
 
         $transactionData = new TransactionData(
             value: 123,
@@ -55,6 +57,33 @@ class CreateTransactionUseCaseTest extends TestCase
             receiver: fake()->uuid()
         );
 
+        $this->expectException(TransactionException::class);
         $this->transactionUseCaseMock->handle($transactionData);
+    }
+
+    public function testCreateTransactionSuccessfuly()
+    {
+        $this->paymentAuthorizerMock->method('isAuthorizerPayment')
+            ->willReturn(true);
+
+        $walletSender = new Wallet();
+        $walletSender->balance = 1999;
+        $walletSender->user = (object) ['user_type' => UserTypeEnum::Customer->value];
+        $walletSender->id = fake()->uuid();
+
+        $this->walletRepositoryMock->method('getWalletById')
+            ->willReturn($walletSender);
+
+        $transactionData = new TransactionData(
+            value: 123,
+            sender: fake()->uuid(),
+            receiver: fake()->uuid()
+        );
+
+        $this->notificationsQueueMock->method('dispatch')
+            ->with('Transaction Created with Successfuly');
+
+        $this->transactionUseCaseMock->handle($transactionData);
+        $this->assertEquals($transactionData, $this->transactionUseCaseMock->handle($transactionData));
     }
 }
